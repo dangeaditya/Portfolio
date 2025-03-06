a33520ae4f4fddc2eacb453b97abda753e2c6678
dangeaditya <126845226+dangeaditya@users.noreply.github.com>
2025-03-06 20:17:09 +0530

    First Commit [Commiting this project]
    I am committing this project to my Github account for first time

---
+++ script.js
@@ -0,0 +1,15 @@
+// Dark Mode Toggle
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
